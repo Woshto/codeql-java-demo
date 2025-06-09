2d72a1e65f4565dccf14a3b1a4ea12b76ed0b953
Wellington <127452908+Woshto@users.noreply.github.com>
2025-06-09 16:32:17 -0300

    Create Vulnerable.java

---
+++ src/Vulnerable.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class Vulnerable {
+    public static void main(String[] args) throws Exception {
+        String userInput = args[0];  // Entrada do usuário
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+        Statement stmt = con.createStatement();
+        String query = "SELECT * FROM users WHERE name = '" + userInput + "'";
+        ResultSet rs = stmt.executeQuery(query);  // Execução insegura
+    }
+}
