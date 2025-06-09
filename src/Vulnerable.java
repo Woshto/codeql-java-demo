d74cad2727aa79e7d06523436a16366ad410f0f1
Wellington <127452908+Woshto@users.noreply.github.com>
2025-06-09 17:22:22 -0300

    Update Vulnerable.java

--- src/Vulnerable.java
+++ src/Vulnerable.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Vulnerable {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine(); // ← entrada vinda do usuário
+        String userInput = scanner.nextLine(); 
 
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
         Statement stmt = con.createStatement();
         // Construção vulnerável da query
-        String query = "SELECT * FROM users WHERE name = '" + userInput + "'"; // SQL Injection aqui
-        System.out.println("Executando query: " + query); // Apenas para demonstração
+        String query = "SELECT * FROM users WHERE name = '" + userInput + "'"; 
+        System.out.println("Executando query: " + query); 
         ResultSet rs = stmt.executeQuery(query);
         
         while (rs.next()) {
